99d87ef18202b93a24b46b9f68f687098ea64785
Nuruzzaman <haquenzaman@gmail.com>
2021-09-20 11:01:44 +0600

    What is your name

---
+++ FirstName_LastName.py
@@ -0,0 +1,7 @@
+def print_full_name(first, last):
+    name = f"Hello {first} {last}! You just delved into python."
+    return name
+
+first_name = input()
+last_name = input()
+print(print_full_name(first_name, last_name))
