46a6fd8a3a1b818a05fb13dcd2c5df401f299473
Nuruzzaman <haquenzaman@gmail.com>
2021-09-28 15:17:11 +0600

    advance  pattern

--- Designer Door Mat.py
+++ Designer Door Mat.py
@@ -1,27 +1,42 @@
-i = 1
-k = 1
-while i <= 7:
-    b = 1
-    while b <= 7 - i:
-        print(" ", end=" ")
-        b = b + 1
-    j = 1
-    while j <= k:
-        print("*", end=" ")
-        j = j + 1
-    k = k + 2
-    print()
-    i = i + 1
-i = 7
-k = 7
-while i > 0:
-    b = 1
-    while b <= k-i:
-        print(" ", end=" ")
-        b = b + 1
-    j = 1
-    while j <= (i * 2) - 1:
-        print("*", end=" ")
-        j = j + 1
-    print()
-    i = i - 1
+N, M = map(int, input().split())
+for i in range(1, N, 2):
+    print((str('.|.') * i).center(M, "-"))
+print("WELCOME".center(M, "-"))
+for i in range(N - 2, -1, -2):
+    print((str('.|.') * i).center(M, "-"))
+
+
+
+# N, M = map(int, input().split())
+# for i in range(1, N, 2):
+#     print((str('*') * i).center(M, " "))
+# #print(("WELCOME").center(M, "-"))
+# for i in range(N - 2, -1, -2):
+#     print((str('*') * i).center(M, " "))
+# i = 1
+# k = 1
+# while i <= 7:
+#     b = 1
+#     while b <= 7 - i:
+#         print(" ", end=" ")
+#         b = b + 1
+#     j = 1
+#     while j <= k:
+#         print("*", end=" ")
+#         j = j + 1
+#     k = k + 2
+#     print()
+#     i = i + 1
+# i = 7
+# k = 7
+# while i > 0:
+#     b = 1
+#     while b <= k-i:
+#         print(" ", end=" ")
+#         b = b + 1
+#     j = 1
+#     while j <= (i * 2) - 1:
+#         print("*", end=" ")
+#         j = j + 1
+#     print()
+#     i = i - 1
