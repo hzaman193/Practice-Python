4c33c2fc69547b8fd6c03484d67d2aecb9020005
Nuruzzaman <haquenzaman@gmail.com>
2021-09-28 16:07:08 +0600

    final Door

--- Designer Door Mat.py
+++ Designer Door Mat.py
@@ -1,9 +1,16 @@
 N, M = map(int, input().split())
-for i in range(1, N, 2):
+i = 1
+while i < N:
     print((str('.|.') * i).center(M, "-"))
+    i = i + 2
 print("WELCOME".center(M, "-"))
 for i in range(N - 2, -1, -2):
     print((str('.|.') * i).center(M, "-"))
+# for i in range(1, N, 2):
+#     print((str('.|.') * i).center(M, "-"))
+# print("WELCOME".center(M, "-"))
+# for i in range(N - 2, -1, -2):
+#     print((str('.|.') * i).center(M, "-"))
 
 
 
