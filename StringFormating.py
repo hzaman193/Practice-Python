04cf9407ae45b9e69650a175559f3165ac6c345b
Nuruzzaman <haquenzaman@gmail.com>
2021-10-12 12:24:07 +0600

    String Formating

---
+++ StringFormating.py
@@ -0,0 +1,5 @@
+n = int(input())
+l = len(bin(n)[2:])
+for i in range(1, n + 1):
+    print(str(i).rjust(l) + " " + oct(i)[2:].rjust(l) + " " + hex(i)[2:].upper().rjust(l) + " " + bin(i)[2:].rjust(l))
+
